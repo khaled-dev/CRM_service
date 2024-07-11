d64fbdc99b59ea3b7a806c015739fad08122f4a9
Khaled AbdElRahim <khaled710017@gmail.com>
2024-07-11 22:14:47 +0300

    Add swagger docs for accounts

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,16 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\Account;
 use Illuminate\Http\Response;
+use OpenApi\Annotations as OA;
 use App\Http\Resources\AccountResource;
 use App\Http\Requests\Account\StoreRequest;
 use App\Http\Requests\Account\UpdateRequest;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
+/**
+ * @OA\Tag(name="Accounts", description="Operations about accounts")
+ */
 class AccountController extends Controller
 {
 
     /**
-     * @return AnonymousResourceCollection
+     * @OA\Get(
+     *     path="/api/accounts",
+     *     tags={"Accounts"},
+     *     summary="List accounts",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Request Successful",
+     *     )
+     * )
      */
     public function index(): AnonymousResourceCollection
     {
@@ -21,8 +33,25 @@ class AccountController extends Controller
     }
 
     /**
-     * @param StoreRequest $request
-     * @return AccountResource
+     * @OA\Post(
+     *     path="/api/accounts",
+     *     tags={"Accounts"},
+     *     summary="Create account",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *              type="object",
+     *              properties={
+     *                  @OA\Property(property="name", type="string", description="The name of the account", maxLength=255),
+     *                  @OA\Property(property="industry", type="string", description="The industry of the account", maxLength=255),
+     *                  @OA\Property(property="annual_revenue", type="string", description="The annual revenue of the account", maxLength=255),
+     *                  @OA\Property(property="status", type="string", description="he status of the account", enum={"active", "inactive"}),
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(response="201", description="User created successfully"),
+     *     @OA\Response(response="400", description="Bad Request")
+     * )
      */
     public function store(StoreRequest $request): AccountResource
     {
@@ -33,8 +62,19 @@ class AccountController extends Controller
     }
 
     /**
-     * @param Account $account
-     * @return AccountResource
+     * @OA\Get(
+     *     path="/api/accounts/{id}",
+     *     tags={"Accounts"},
+     *     summary="Show account",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(response=200, description="Request Successful"),
+     *     @OA\Response(response=404, description="Page Not Found")
+     * )
      */
     public function show(Account $account): AccountResource
     {
@@ -42,9 +82,31 @@ class AccountController extends Controller
     }
 
     /**
-     * @param UpdateRequest $request
-     * @param Account $account
-     * @return AccountResource
+     * @OA\Put(
+     *     path="/api/accounts/{id}",
+     *     tags={"Accounts"},
+     *     summary="Update account",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=false,
+     *         @OA\JsonContent(
+     *              type="object",
+     *              properties={
+     *                  @OA\Property(property="name", type="string", description="The name of the account", maxLength=255),
+     *                  @OA\Property(property="industry", type="string", description="The industry of the account", maxLength=255),
+     *                  @OA\Property(property="annual_revenue", type="string", description="The annual revenue of the account", maxLength=255),
+     *                  @OA\Property(property="status", type="string", description="he status of the account", enum={"active", "inactive"}),
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(response="201", description="User created successfully"),
+     *     @OA\Response(response="400", description="Bad Request")
+     * )
      */
     public function update(UpdateRequest $request, Account $account): AccountResource
     {
@@ -54,8 +116,18 @@ class AccountController extends Controller
     }
 
     /**
-     * @param Account $account
-     * @return Response
+     * @OA\Delete(
+     *     path="/api/accounts/{id}",
+     *     tags={"Accounts"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(response=200, description="Request Successful"),
+     *     @OA\Response(response=404, description="Page Not Found")
+     * )
      */
     public function destroy(Account $account): Response
     {
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="My API",
+ *     version="1.0",
+ *     description="A sample API to demonstrate usage of OpenAPI annotations."
+ * )
+ */
 
 abstract class Controller
 {
