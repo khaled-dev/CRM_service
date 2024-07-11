5e9908c43e05fa2f55db4a6c9173714282d48f00
Khaled AbdElRahim <khaled710017@gmail.com>
2024-07-11 22:49:40 +0300

    Add customer swagger notation

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use Illuminate\Http\Response;
+use OpenApi\Annotations as OA;
 use App\Http\Requests\CommentRequest;
 use App\Http\Resources\RequestResource;
 use App\Http\Resources\CommentResource;
@@ -13,10 +14,19 @@ use App\Http\Requests\Customer\UpdateRequest;
 use App\Http\Requests\Customer\RequestRequest;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
+/**
+ * @OA\Tag(name="Customers", description="Operations about customers")
+ */
 class CustomerController extends Controller
 {
     /**
-     * @return AnonymousResourceCollection
+     * @OA\Get(
+     *     path="/api/customers",
+     *     tags={"Customers"},
+     *     summary="List activities",
+     *     @OA\Response(response="201", description="Request Successful"),
+     *     @OA\Response(response="400", description="Bad Request")
+     * )
      */
     public function index(): AnonymousResourceCollection
     {
@@ -24,8 +34,29 @@ class CustomerController extends Controller
     }
 
     /**
-     * @param StoreRequest $request
-     * @return CustomerResource
+     * @OA\Post(
+     *     path="/api/customers",
+     *     tags={"Customers"},
+     *     summary="Create customer",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *              type="object",
+     *              properties={
+     *                  @OA\Property(property="name", type="string", description="The name of the customer", maxLength=255),
+     *                  @OA\Property(property="company_name", type="string", description="The company_name of the customer", maxLength=255),
+     *                  @OA\Property(property="address", type="string", description="The address of the customer", maxLength=255),
+     *                  @OA\Property(property="phone", type="string", description="The phone of the customer", maxLength=255),
+     *                  @OA\Property(property="email", type="string", description="the email of the customer", maxLength=255),
+     *                  @OA\Property(property="country", type="string", description="the country of the customer", maxLength=255),
+     *                  @OA\Property(property="status", type="string", description="the status of the customer", enum={"potential", "current"}),
+     *                  @OA\Property(property="type", type="string", description="the type of the customer", maxLength=255),
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(response="201", description="Request Successful"),
+     *     @OA\Response(response="400", description="Bad Request")
+     * )
      */
     public function store(StoreRequest $request): CustomerResource
     {
@@ -36,9 +67,28 @@ class CustomerController extends Controller
     }
 
     /**
-     * @param CommentRequest $request
-     * @param Customer $customer
-     * @return CommentResource
+     * @OA\Post(
+     *     path="/api/customers/{id}/comments",
+     *     tags={"Customers"},
+     *     summary="Create comment on a customer",
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          @OA\Schema(type="string")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *              type="object",
+     *              properties={
+     *                  @OA\Property(property="comment", type="string", description="The comment on the customer", maxLength=255)
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(response="201", description="Request Successful"),
+     *     @OA\Response(response="400", description="Bad Request")
+     * )
      */
     public function addComment(CommentRequest $request, Customer $customer): CommentResource
     {
@@ -48,9 +98,28 @@ class CustomerController extends Controller
     }
 
     /**
-     * @param RequestRequest $request
-     * @param Customer $customer
-     * @return RequestResource
+     * @OA\Post(
+     *     path="/api/customers/{id}/requests",
+     *     tags={"Customers"},
+     *     summary="Create request on a customer",
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          @OA\Schema(type="string")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *              type="object",
+     *              properties={
+     *                  @OA\Property(property="request", type="string", description="The request on the customer", maxLength=255)
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(response="201", description="Request Successful"),
+     *     @OA\Response(response="400", description="Bad Request")
+     * )
      */
     public function addRequest(RequestRequest $request, Customer $customer): RequestResource
     {
@@ -60,8 +129,19 @@ class CustomerController extends Controller
     }
 
     /**
-     * @param Customer $customer
-     * @return CustomerResource
+     * @OA\Get(
+     *     path="/api/customers/{id}",
+     *     tags={"Customers"},
+     *     summary="Show activity",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(response=200, description="Request Successful"),
+     *     @OA\Response(response=404, description="Page Not Found")
+     * )
      */
     public function show(Customer $customer): CustomerResource
     {
@@ -69,9 +149,35 @@ class CustomerController extends Controller
     }
 
     /**
-     * @param UpdateRequest $request
-     * @param Customer $customer
-     * @return CustomerResource
+     * @OA\Put(
+     *     path="/api/customers/{id}",
+     *     tags={"Customers"},
+     *     summary="Update customer",
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          @OA\Schema(type="string")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=false,
+     *         @OA\JsonContent(
+     *              type="object",
+     *              properties={
+     *                  @OA\Property(property="name", type="string", description="The name of the customer", maxLength=255),
+     *                  @OA\Property(property="company_name", type="string", description="The company_name of the customer", maxLength=255),
+     *                  @OA\Property(property="address", type="string", description="The address of the customer", maxLength=255),
+     *                  @OA\Property(property="phone", type="string", description="The phone of the customer", maxLength=255),
+     *                  @OA\Property(property="email", type="string", description="the email of the customer", maxLength=255),
+     *                  @OA\Property(property="country", type="string", description="the country of the customer", maxLength=255),
+     *                  @OA\Property(property="status", type="string", description="the status of the customer", enum={"potential", "current"}),
+     *                  @OA\Property(property="type", type="string", description="the type of the customer", maxLength=255),
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(response="201", description="Request Successful"),
+     *     @OA\Response(response="400", description="Bad Request")
+     * )
      */
     public function update(UpdateRequest $request, Customer $customer): CustomerResource
     {
@@ -81,8 +187,18 @@ class CustomerController extends Controller
     }
 
     /**
-     * @param Customer $customer
-     * @return Response
+     * @OA\Delete(
+     *     path="/api/customers/{id}",
+     *     tags={"Customers"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(response=200, description="Request Successful"),
+     *     @OA\Response(response=404, description="Page Not Found")
+     * )
      */
     public function destroy(Customer $customer): Response
     {
