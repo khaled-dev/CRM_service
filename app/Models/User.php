d11d70cd0b6f6412283b375f072e5fef2c8d1e3b
Khaled AbdElRahim <khaled710017@gmail.com>
2024-07-16 00:58:38 +0300

    Update user model and add its resource

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
 
     public const SALESMAN = 'salesman';
 
-    public const STATUS = [self::ADMIN, self::ACCOUNT_MANGER, self::SALESMAN];
+    public const TYPE = [self::ADMIN, self::ACCOUNT_MANGER, self::SALESMAN];
 
     /**
      * The attributes that are mass assignable.
@@ -28,6 +28,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'type',
         'password',
     ];
 
